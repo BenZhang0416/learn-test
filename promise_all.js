aaba97be472ca78f84f91b3b6b4851b526ce7be6
zhangben <benzhang0416@163.com>
2022-02-16 20:34:52 +0800

    20220216

--- promise_all.js
+++ promise_all.js
@@ -1,3 +1,53 @@
+// 1、promiseAll 接受一个 promises 数组
+// 2、只有数组中的 promise 全部变为 resolve 的时候
+// 3、返回一个 promise 实例
+// 4、只要有一个失败，状态就变为 rejected
+
+
+// 箭头函数实现
+const promiseAll = promisesArr => {
+    let ret = [];
+    const len = promisesArr.length;
+    let cnt = 0;
+    return new Promise((resolve, reject) => {
+      for (let val of promisesArr) {
+        Promise.resolve(val).then((res) => {
+          cnt++; 
+          ret.push(res);
+          if (cnt === len) {
+            return resolve(ret);
+          }
+        },(err) => reject(err));
+      }
+    })
+  } 
+  
+  // 在 promise 实例上新增一个 promiseAll 方法
+  Promise.prototype.promiseAll1 = promiseAll;
+  
+  const pro1 = new Promise((res,rej) => {
+    setTimeout(() => {
+      res('1')
+    },1000)
+  })
+  const pro2 = new Promise((res,rej) => {
+    setTimeout(() => {
+      res('2')
+    },2000)
+  })
+  const pro3 = new Promise((res,rej) => {
+    setTimeout(() => {
+      res('3')
+    },3000)
+  })
+  
+  const proAll = promiseAll([pro1,pro2,pro3])
+  .then(res => 
+    console.log(res) // 3秒之后打印 ["1", "2", "3"]
+  )
+  
+
+
 function pAll(_promises) { 
     //_promises 是迭代器
     return new Promise((resolve, reject) => {
